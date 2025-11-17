4728921e6fd2aceefa5643e73ec6cad4b33165ef
dhurvshah1102 <shahdhruv812@gmail.com>
2025-11-17 23:47:52 +0530

    logo color change

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import logo from '../images/white 1.png';
+import logo from '../images/Black 1.png';
 import Image from "next/image";
 import Link from "next/link";
 
