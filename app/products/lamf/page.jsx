3c5ea6bbbc86dd2fafb8a7fcb13b1d2b1883aca9
DEBAPALLAB <Dasadi0706@gmail.com>
2025-12-09 23:16:01 +0530

    push

--- app/products/lamf/page.jsx
+++ app/products/lamf/page.jsx
@@ -359,13 +359,7 @@ export default function LAMFPage() {
       </th>
     ))}
 
-    {/* Contact */}
-    <th
-      style={{ background: "#124434", color: "#FFFFFF" }}
-      className="px-5 py-4 border border-gray-300 uppercase text-sm tracking-wide"
-    >
-      Contact
-    </th>
+    
 
   </tr>
 </thead>
